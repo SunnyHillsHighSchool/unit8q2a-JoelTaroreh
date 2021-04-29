797c8585b9ddf5a5937e8eaa12eb19db30f4761e
Joel Taroreh <>
2021-04-29 05:55:55 +0000

    complete

--- SongWord.java
+++ SongWord.java
@@ -2,8 +2,15 @@ public class SongWord
 {
   public int findCount(String[][]songArray, String word)
   {
+    int count = 0;
+    for(String[] arr: songArray) { 
+      for(String song: arr){
+        if(song.indexOf(word) >= 0) {
+          count++;
+        }
+      }
+    }
 
-
-    return 0;
+    return count;
   }
 }
